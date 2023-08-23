c8cacdc09fe496d5241a7785311773419d1d9a22
Ray-D-Song <songraysmail@gmail.com>
2023-08-23 21:06:05 +0800

    feat: loading

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator>
-      <div class="text-3xl">
-        Just wait a moment...
+      <div class="w-screen h-screen blur-sm flex flex-col items-center justify-center z-50" :color="loadingBarColor">
+        <div class="text-3xl">
+          Just wait a moment...
+        </div>
       </div>
     </NuxtLoadingIndicator>
     <NuxtPage />
@@ -12,4 +14,9 @@
 <script setup lang="ts">
 import './app.css'
 import './assets/global.scss'
+
+const colorMode = useColorMode()
+const loadingBarColor = computed(() => {
+  return colorMode.preference === 'light'?'black':'white'
+})
 </script>
