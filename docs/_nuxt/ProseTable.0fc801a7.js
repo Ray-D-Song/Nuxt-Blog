821601c6da0ad47181138b6fa9b2e011483928a2
Ray <songraysmail@gmail.com>
2023-08-24 15:26:10 +0800

    fix: sitemap

---
+++ docs/_nuxt/ProseTable.0fc801a7.js
@@ -0,0 +1,1 @@
+import{Q as o,o as r,c as t,m as s}from"./entry.366d4247.js";const a={};function c(e,n){return r(),t("table",null,[s(e.$slots,"default")])}const f=o(a,[["render",c]]);export{f as default};
--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,23 +1,41 @@
 import { serverQueryContent } from '#content/server'
-import { timeStamp } from 'console'
-import { SitemapStream, streamToPromise } from 'sitemap'
+
+const formatDate = (param: number) => {
+    const date = new Date(param)
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
+}
 
 export default defineEventHandler(async (event) => {
   const docs = await serverQueryContent(event).find()
-  const sitemap = new SitemapStream({
-    hostname: 'https://ray-d-song.com'
-  })
+  const currentDate = (new Date()).getTime()
 
-  sitemap.write({
-    url: '',
-  })
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+  const lastmod = formatDate(currentDate)
+  xml += `
+    <url>
+      <loc>https://ray-d-song.com</loc>
+      <lastmod>${lastmod}</lastmod>
+      <changefreq>monthly</changefreq>
+    </url>`
+
   for (const doc of docs) {
-    sitemap.write({
-      url: 'post' + `${doc._path}`,
-      lastod: Number(doc._path?.substr(1))
-    })
-  }
-  sitemap.end()
+    const formattedDate = formatDate(Number(doc._path?.substr(1)))
+    const str = `
+    <url>
+      <loc>https://ray-d-song.com/post${doc._path}</loc>
+      <lastmod>${formattedDate}</lastmod>
+      <changefreq>monthly</changefreq>
+    </url>\n`
 
-  return streamToPromise(sitemap)
+    xml += str
+  }
+
+  xml += '</urlset>'
+  return Buffer.from(xml, 'utf-8')
 })
+
