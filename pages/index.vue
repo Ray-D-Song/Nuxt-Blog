0623e9541791f358c5cd17cefa48011994b447e9
Ray <songraysmail@gmail.com>
2024-01-10 12:38:38 +0800

    style improve

--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,8 @@
     </div>
     <div class="hidden md:block markdown fixed sm:w-2/6 lg:w-2/5 bottom-0 right-20 h-screen">
       <div class="h-full flex flex-col justify-center">
-        <div class="text-3xl">Hi, this is <span class="italic underline">Ray-D-Song</span>'s Blog.</div>
+        <div class="text-3xl italic">Ray-D-Song</div>
         <div class="text-xl my-5">My primary job is a web front-end development engineer. <br/> But I also dabble in various other technologies.</div>
-        <div class="text-xl">You can click the icon button to subscribe to the RSS feed or check out my GitHub.</div>
       </div>
     </div>
   </div>
